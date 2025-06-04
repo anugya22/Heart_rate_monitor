f6445c0629f4da4d26af74a88c9ab23be5de1b64
anugya22 <141493025+anugya22@users.noreply.github.com>
2025-06-04 15:09:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,6 @@ def result():
     return render_template("result.html", bpm=bpm, smoothed=smoothed, peaks=peaks)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
